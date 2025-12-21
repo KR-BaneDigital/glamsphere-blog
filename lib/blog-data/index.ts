9108cde7a3902cc10fc0cc6abd5f28e987eaa09a
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-21 17:25:23 -0500

    Append nyx-epic-ink-liner-review-the-best-waterproof-vegan-eyeliner to blog index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -3,9 +3,11 @@
 
 import { post as est_e_lauder_futurist_hydra_rescue_foundation_12_hour_glow_r } from './est-e-lauder-futurist-hydra-rescue-foundation-12-hour-glow-r.data'
 import { post as discover_laura_geller_s_best_of_the_best_baked_palette_tusca } from './discover-laura-geller-s-best-of-the-best-baked-palette-tusca.data'
+import { post as nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner } from './nyx-epic-ink-liner-review-the-best-waterproof-vegan-eyeliner.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner,
   discover_laura_geller_s_best_of_the_best_baked_palette_tusca,
   est_e_lauder_futurist_hydra_rescue_foundation_12_hour_glow_r
 ]

5c0abaf3614309e8f8538406aa20d2327eef2bf1
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-21 20:06:31 -0500

    Append una-gella-almond-nail-tips-review-216pcs-medium-press-on-per to blog index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -4,9 +4,11 @@
 import { post as est_e_lauder_futurist_hydra_rescue_foundation_12_hour_glow_r } from './est-e-lauder-futurist-hydra-rescue-foundation-12-hour-glow-r.data'
 import { post as discover_laura_geller_s_best_of_the_best_baked_palette_tusca } from './discover-laura-geller-s-best-of-the-best-baked-palette-tusca.data'
 import { post as nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner } from './nyx-epic-ink-liner-review-the-best-waterproof-vegan-eyeliner.data'
+import { post as una_gella_almond_nail_tips_review_216pcs_medium_press_on_per } from './una-gella-almond-nail-tips-review-216pcs-medium-press-on-per.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  una_gella_almond_nail_tips_review_216pcs_medium_press_on_per,
   nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner,
   discover_laura_geller_s_best_of_the_best_baked_palette_tusca,
   est_e_lauder_futurist_hydra_rescue_foundation_12_hour_glow_r

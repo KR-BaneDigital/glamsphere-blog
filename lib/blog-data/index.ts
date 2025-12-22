1d196e4f4b789842b4be3ec1522ba02613ac56e2
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-21 19:25:36 -0500

    Append saie-slip-tint-moisturizer-spf-35-your-essential-summer-glow to blog index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -4,9 +4,11 @@
 import { post as est_e_lauder_futurist_hydra_rescue_foundation_12_hour_glow_r } from './est-e-lauder-futurist-hydra-rescue-foundation-12-hour-glow-r.data'
 import { post as discover_laura_geller_s_best_of_the_best_baked_palette_tusca } from './discover-laura-geller-s-best-of-the-best-baked-palette-tusca.data'
 import { post as nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner } from './nyx-epic-ink-liner-review-the-best-waterproof-vegan-eyeliner.data'
+import { post as saie_slip_tint_moisturizer_spf_35_your_essential_summer_glow } from './saie-slip-tint-moisturizer-spf-35-your-essential-summer-glow.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  saie_slip_tint_moisturizer_spf_35_your_essential_summer_glow,
   nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner,
   discover_laura_geller_s_best_of_the_best_baked_palette_tusca,
   est_e_lauder_futurist_hydra_rescue_foundation_12_hour_glow_r

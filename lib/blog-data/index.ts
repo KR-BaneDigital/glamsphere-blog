1310867c98515432277a2752ff1ce2c780415cbc
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-21 20:47:25 -0500

    Append nail-prep-set-unlock-salon-quality-manicures-at-home to blog index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -6,9 +6,11 @@ import { post as discover_laura_geller_s_best_of_the_best_baked_palette_tusca } 
 import { post as nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner } from './nyx-epic-ink-liner-review-the-best-waterproof-vegan-eyeliner.data'
 import { post as una_gella_almond_nail_tips_review_216pcs_medium_press_on_per } from './una-gella-almond-nail-tips-review-216pcs-medium-press-on-per.data'
 import { post as bareminerals_original_mineral_veil_the_ultimate_talc_free_se } from './bareminerals-original-mineral-veil-the-ultimate-talc-free-se.data'
+import { post as nail_prep_set_unlock_salon_quality_manicures_at_home } from './nail-prep-set-unlock-salon-quality-manicures-at-home.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  nail_prep_set_unlock_salon_quality_manicures_at_home,
   bareminerals_original_mineral_veil_the_ultimate_talc_free_se,
   una_gella_almond_nail_tips_review_216pcs_medium_press_on_per,
   nyx_epic_ink_liner_review_the_best_waterproof_vegan_eyeliner,

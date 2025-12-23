01a9385616c93996e4e5f630efa25975c1bacc54
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-23 05:02:06 -0500

    [Auto-Publish] Index: long-straight-nails-unlock-the-secret-to-effortlessly-chic-m

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -36,9 +36,11 @@ import { post as unlock_silky_hair_the_biosilk_duo_set_that_transforms_your_r } 
 import { post as short_almond_tips_elevate_your_nail_game_with_effortless_ele } from './short-almond-tips-elevate-your-nail-game-with-effortless-ele.data'
 import { post as press_on_nails_instant_glam_for_effortless_stunning_manicure } from './press-on-nails-instant-glam-for-effortless-stunning-manicure.data'
 import { post as unlock_silky_hair_why_aussie_miracle_moist_is_your_secret_we } from './unlock-silky-hair-why-aussie-miracle-moist-is-your-secret-we.data'
+import { post as long_straight_nails_unlock_the_secret_to_effortlessly_chic_m } from './long-straight-nails-unlock-the-secret-to-effortlessly-chic-m.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  long_straight_nails_unlock_the_secret_to_effortlessly_chic_m,
   unlock_silky_hair_why_aussie_miracle_moist_is_your_secret_we,
   press_on_nails_instant_glam_for_effortless_stunning_manicure,
   short_almond_tips_elevate_your_nail_game_with_effortless_ele,

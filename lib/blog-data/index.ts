1360f9e094592ee48de466b1fefc7db6083eb072
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-23 03:02:33 -0500

    [Auto-Publish] Index: press-on-nails-instant-glam-for-effortless-stunning-manicure

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -34,9 +34,11 @@ import { post as unveiling_radiance_why_rose_garden_gel_is_your_skin_s_new_be } 
 import { post as glamermaid_nails_unlock_the_secret_to_stunning_salon_quality } from './glamermaid-nails-unlock-the-secret-to-stunning-salon-quality.data'
 import { post as unlock_silky_hair_the_biosilk_duo_set_that_transforms_your_r } from './unlock-silky-hair-the-biosilk-duo-set-that-transforms-your-r.data'
 import { post as short_almond_tips_elevate_your_nail_game_with_effortless_ele } from './short-almond-tips-elevate-your-nail-game-with-effortless-ele.data'
+import { post as press_on_nails_instant_glam_for_effortless_stunning_manicure } from './press-on-nails-instant-glam-for-effortless-stunning-manicure.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  press_on_nails_instant_glam_for_effortless_stunning_manicure,
   short_almond_tips_elevate_your_nail_game_with_effortless_ele,
   unlock_silky_hair_the_biosilk_duo_set_that_transforms_your_r,
   glamermaid_nails_unlock_the_secret_to_stunning_salon_quality,

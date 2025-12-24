f3e3d2be54d33b2c399a96b9c61c30b26809e78a
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-24 15:46:48 -0500

    fix: Add Search Console verification token for https://glamsphere.org

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,12 @@ const robotoSlab = Roboto_Slab({
 })
 
 export const metadata: Metadata = {
-  // Google Search Console Verification
   verification: {
     google: '<meta name="google-site-verification" content="x4_CRmoaRuXW5q1kmc_I2GMPar5J2aHAdD2J6pqhC0Y" />'
+  },
+  // Google Search Console Verification
+  verification: {
+    google: ''
   },
   title: `${siteConfig.brand.name} | Trusted Insights and Expert Content`,
   description: siteConfig.brand.description,

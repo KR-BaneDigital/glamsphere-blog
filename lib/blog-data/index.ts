613b34eff9d5d45a3de9fd0a24c7f49a28e69048
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-25 01:03:52 -0500

    [Auto-Publish] Index: unlock-endless-looks-why-la-palette-nude-is-your-beauty-game

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -78,9 +78,11 @@ import { post as unlock_luxurious_locks_the_kundal_shampoo_set_you_can_t_miss } 
 import { post as lilash_serum_the_secret_to_luscious_lashes_you_can_t_ignore } from './lilash-serum-the-secret-to-luscious-lashes-you-can-t-ignore.data'
 import { post as unlock_luxurious_locks_kristin_ess_shampoo_review_for_shiny_ } from './unlock-luxurious-locks-kristin-ess-shampoo-review-for-shiny-.data'
 import { post as olaplex_hair_kit_unlock_your_hair_s_true_potential_for_gorge } from './olaplex-hair-kit-unlock-your-hair-s-true-potential-for-gorge.data'
+import { post as unlock_endless_looks_why_la_palette_nude_is_your_beauty_game } from './unlock-endless-looks-why-la-palette-nude-is-your-beauty-game.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  unlock_endless_looks_why_la_palette_nude_is_your_beauty_game,
   olaplex_hair_kit_unlock_your_hair_s_true_potential_for_gorge,
   unlock_luxurious_locks_kristin_ess_shampoo_review_for_shiny_,
   lilash_serum_the_secret_to_luscious_lashes_you_can_t_ignore,

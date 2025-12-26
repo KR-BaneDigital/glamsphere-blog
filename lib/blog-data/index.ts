eb446ab6cb3e6837ff3c7b3d35e4fec62cb822d1
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-26 07:02:01 -0500

    [Auto-Publish] Index: unlock-luxurious-locks-oribe-supershine-set-review-for-radia

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -112,9 +112,11 @@ import { post as unlock_your_hair_s_hydration_the_magic_of_biolage_hydra_sour } 
 import { post as unlock_luscious_locks_a_deep_dive_into_the_hair_growth_set } from './unlock-luscious-locks-a-deep-dive-into-the-hair-growth-set.data'
 import { post as unlock_gorgeous_curls_the_curl_stretch_pack_that_delivers } from './unlock-gorgeous-curls-the-curl-stretch-pack-that-delivers.data'
 import { post as unlock_stunning_lashes_the_lash_serum_that_transforms_your_l } from './unlock-stunning-lashes-the-lash-serum-that-transforms-your-l.data'
+import { post as unlock_luxurious_locks_oribe_supershine_set_review_for_radia } from './unlock-luxurious-locks-oribe-supershine-set-review-for-radia.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  unlock_luxurious_locks_oribe_supershine_set_review_for_radia,
   unlock_stunning_lashes_the_lash_serum_that_transforms_your_l,
   unlock_gorgeous_curls_the_curl_stretch_pack_that_delivers,
   unlock_luscious_locks_a_deep_dive_into_the_hair_growth_set,

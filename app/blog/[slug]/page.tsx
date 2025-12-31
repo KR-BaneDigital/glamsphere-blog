935c95adc1ff2777b63b762747bf939226bf75f3
Kyle Richardson <kyle@banedigital.com>
2025-12-31 00:31:45 -0500

    fix: correct OpenGraph images indentation in blog post metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -51,12 +51,12 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
       type: "article",
       publishedTime: post.date,
       authors: [post.author.name],
-    images: post.image ? [{
-      url: post.image,
-      width: 1200,
-      height: 630,
-      alt: post.title,
-    }] : undefined,
+      images: post.image ? [{
+        url: post.image,
+        width: 1200,
+        height: 630,
+        alt: post.title,
+      }] : undefined,
     },
     twitter: {
       card: "summary_large_image",

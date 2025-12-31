c2df7cb727f2cb67246435a1868aedff14449b93
Kyle Richardson <kyle@banedigital.com>
2025-12-30 20:57:11 -0500

    Fix: Use hero image for og:image instead of logo

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -51,13 +51,18 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
       type: "article",
       publishedTime: post.date,
       authors: [post.author.name],
-      images: [post.image],
+      images: post.image ? [{
+        url: `${siteConfig.brand.url}${post.image}`,
+        width: 1200,
+        height: 630,
+        alt: post.title,
+      }] : undefined,
     },
     twitter: {
       card: "summary_large_image",
       title: post.title,
       description: post.excerpt,
-      images: [post.image],
+      images: post.image ? [`${siteConfig.brand.url}${post.image}`] : undefined,
     },
   }
 }

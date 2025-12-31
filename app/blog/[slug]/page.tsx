f4240eddb8f4022bd83de5d4c5f05bdfd7fa7203
Kyle Richardson <kyle@banedigital.com>
2025-12-31 13:21:57 -0500

    Fix: Add og:image and twitter:image meta tags for blog posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -42,8 +42,13 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
     }
   }
 
+  // Construct absolute image URL
+  const absoluteImageUrl = post.image?.startsWith('http') 
+    ? post.image 
+    : `${siteConfig.brand.url}${post.image}`
+
   return {
-    title: `${post.title} | PureWell Living`,
+    title: `${post.title} | ${siteConfig.brand.name}`,
     description: post.excerpt,
     openGraph: {
       title: post.title,
@@ -51,18 +56,13 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
       type: "article",
       publishedTime: post.date,
       authors: [post.author.name],
-      images: post.image ? [{
-        url: post.image,
-        width: 1200,
-        height: 630,
-        alt: post.title,
-      }] : undefined,
+      images: post.image ? [absoluteImageUrl] : undefined,
     },
     twitter: {
       card: "summary_large_image",
       title: post.title,
       description: post.excerpt,
-      images: post.image ? [post.image] : undefined,
+      images: post.image ? [absoluteImageUrl] : undefined,
     },
   }
 }
